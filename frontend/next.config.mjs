306bedd2691a5033ed8d469fe1a7c4425075758f
Rubens Dias <rubens.dias08@hotmail.com>
2025-04-13 11:30:56 -0300

    next config api url

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ['image.tmdb.org'],
     },
+    env: {
+        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    },
 };
 
 export default nextConfig;
